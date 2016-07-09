2a876ba3a447ad7ad94a3aefc10286f29ed1fe95
handsomedong <646970309@qq.com>
2016-07-10 01:32:37 +0800

    add file

--- charing/settings.py
+++ charing/settings.py
@@ -120,9 +120,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+# STATIC_ROOT=os.path.join(BASE_DIR,'static')
+STATICFILES_DIRS = (os.path.join(BASE_DIR,'static'),)
 
 TEMPLATE_DIRS = (
     os.path.join(os.path.dirname(__file__), '../static').replace('\\','/'),
 )
-
-print(TEMPLATE_DIRS)
--- index/views.py
+++ index/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 
 
-
-
+num=0
 def main(request):
-    return render_to_response("index.html",{"staticmessage":"Hello, I'm ruirui"})
+    global num
+    num=num+1
+    return render_to_response("index.html",{"STATIC_URL":"/static/","number":num})
