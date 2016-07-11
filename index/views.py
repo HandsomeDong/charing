e4661a8da36141424e4cc50618b91657d7564aeb
unknown <460572315@qq.com>
2016-07-12 00:55:52 +0800

    first time~~

--- index/views.py
+++ index/views.py
@@ -4,7 +4,13 @@ from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 
 
 num=0
+
 def main(request):
     global num
+    name=''
+    if 'name' in request.GET:
+        name=request.GET['name']
     num=num+1
-    return render_to_response("index.html",{"STATIC_URL":"/static/","number":num})
+    num=100
+    return render_to_response("index.html",{"STATIC_URL":"/static/","number":num,"name":name})
+
