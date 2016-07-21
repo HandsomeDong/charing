0d5fa58bb44d4f2d8856af1291253788225ac94a
handsomeDong <646970309@qq.com>
2016-07-21 17:44:06 +0800

    modified

--- static/js/register.js
+++ static/js/register.js
@@ -1,7 +1,11 @@
 $(function(){
 
-if ($('.am-form').validator().data('amui.validator').isFormValid()) {
-alert("yes")
-}　　　　　　
+   $.when($('#form1').validator('isFormValid')).then(function() {
+// 验证成功的逻辑
+alert('ok');
+}, function() {
+// 验证失败的逻辑
+alert('no');
+});　　
 
 })
