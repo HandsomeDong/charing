d78fee85a7553d3203ea64c047b98a6abe9d762e
sr <460572315@qq.com>
2016-08-14 17:46:28 +0800

    注册界面

--- static/js/register.js
+++ static/js/register.js
@@ -11,4 +11,36 @@ $('#myButton').on('click', submit_register)
 		});
   }
 
+
+    $("#signIn").on("click",function(){
+        var $mailbox_name=$("#mailbox_name");
+        var $mailbox_key=$("#mailbox_key");
+        var flag=true;
+        var mailbox_name=$mailbox_name.val();
+        var mailbox_key=$mailbox_key.val();
+        var pa=/^[0-9_a-zA-Z]{6,20}$/;
+        // console.log(mailbox_name.length);
+        $mailbox_name.parent().removeClass("has-error");
+        $mailbox_key.parent().removeClass("has-error");
+        if(mailbox_name.length<4||mailbox_name.length>30){
+            //mailbox name check failed
+            flag=false;
+            $mailbox_name.parent().addClass("has-error");
+        }
+        if(!pa.test(mailbox_key)){
+            //mailbox key check failed
+            flag=false;
+            $mailbox_key.parent().addClass("has-error");
+        }
+        if(flag){
+            $.post('/submit_register/', { address: mailbox_name,password: mailbox_key}, function (stat) {
+                console.log(stat["register_success"]);
+                if(!stat["register_success"]){
+                $("#mailbox-name-alert").show();
+                }
+            });
+        }
+
+    });
+
 })
