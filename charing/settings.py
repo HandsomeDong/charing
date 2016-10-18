b78f45c829136bc268f8d66223b0bb4119f41828
handsomeDong <646970309@qq.com>
2016-10-18 16:33:39 +0800

    添加了用户注册功能(修改按钮)

--- charing/settings.py
+++ charing/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'NAME': 'charing',
         'USER': 'webserver',
         'PASSWORD': '940430',
-        'HOST': '121.201.15.28',
+        'HOST': 'localhost',
         'PORT': '3306',
     }
 }
