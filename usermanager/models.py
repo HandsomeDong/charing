e9a8c6201fdbbe7ce98db45cb19c8d12f1030d46
handsomeDong <646970309@qq.com>
2016-10-18 18:05:46 +0800

    添加了用户注册功能(修改按钮)

--- usermanager/models.py
+++ usermanager/models.py
@@ -2,6 +2,12 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class Mail(models.Model):
+    title = models.CharField(max_length=80)
+    content = models.CharField(max_length=3000)
+    opened = models.BooleanField(default=False)
+
+
 class MailBox(models.Model):
     def __init__(self):
         Mail.objects.filter(title='88')
@@ -11,10 +17,7 @@ class MailBox(models.Model):
     send_box = models.ManyToManyField(Mail)
 
 
-class Mail(models.Model):
-    title = models.CharField(max_length=80)
-    content = models.CharField(max_length=3000)
-    opened = models.BooleanField(default=False)
+
 
 
 
