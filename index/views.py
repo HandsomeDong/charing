bcb635618968cd03400ea0a7e839f47e17a65200
HandsomeDong <646970309@qq.com>
2016-10-20 23:28:16 +0800

    User Models

--- index/views.py
+++ index/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render, render_to_response
 from django.http import HttpResponse, JsonResponse, HttpResponseRedirect
 
+num = 0
 
-num=0
 
 def main(request):
     global num
-    name=''
+    name = ''
     if 'name' in request.GET:
-        name=request.GET['name']
-    num=num+1
-    num=100
+        name = request.GET['name']
+    num += 1
+    num = 100
     #
     # p = Person(name="Fred Flintstone", shirt_size="L")
     # p.save()
-    return render_to_response("index.html",{"STATIC_URL":"/static/","number":num,"name":name})
+    return render_to_response("index.html", {"STATIC_URL": "/static/", "number": num, "name": name})
 
 
