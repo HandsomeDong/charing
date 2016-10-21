2a3467bca6f8e764a2d25c81b4642fe9a68370b0
HandsomeDong <646970309@qq.com>
2016-10-21 16:22:20 +0800

    User Models

--- usermanager/models.py
+++ usermanager/models.py
@@ -2,20 +2,19 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+class MailBox(models.Model):
+    belong = models.ForeignKey(User)
+
+
 class Mail(models.Model):
     title = models.CharField(max_length=80)
     content = models.CharField(max_length=3000)
     opened = models.BooleanField(default=False)
+    receiver = models.ForeignKey(MailBox, related_name='receiver')
+    sender = models.ForeignKey(MailBox, related_name='sender', default=-1)
 
-
-class MailBox(models.Model):
-    def __init__(self):
-        Mail.objects.filter(title='88')
-    belong = models.ForeignKey(User)
-    address = models.CharField(max_length=30)
-    receive_box = models.ManyToManyField(Mail)
-    send_box = models.ManyToManyField(Mail)
-
+if __name__ == '__main__':
+  mailBox = MailBox
 
 
 
